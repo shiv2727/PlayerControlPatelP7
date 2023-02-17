9dee16ac141573faa6d4f26b3821f26b3e6fc080
shiv2727 <720151@student.dusd.net>
2023-02-17 15:27:07 -0800

    Finished car movement
    finished the car movement and adjusted the mass on my objects.

--- PlayerControlPatelP7/Assets/Scripts/PlayerController.cs
+++ PlayerControlPatelP7/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
